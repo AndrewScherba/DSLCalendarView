48d1debc22c2b6aac9004d84b104404388cc6dec
Amol Chavan <amolchavan0619@gmail.com>
2015-01-12 17:06:05 +0530

    selected random date

--- Example/DSLCalendarViewExample/ViewController.m
+++ Example/DSLCalendarViewExample/ViewController.m
@@ -1,36 +1,38 @@
 //
 //  ViewController.m
-//  DSLCalendarViewExample
+//  WBCalender
 //
-//  Created by Pete Callaway on 12/08/2012.
-//  Copyright (c) 2012 Pete Callaway. All rights reserved.
+//  Created by Amol Chavan on 14/10/14.
+//  Copyright (c) 2014 Mansi Technologies Pvt. Ltd. All rights reserved.
 //
 
-#import "DSLCalendarView.h"
 #import "ViewController.h"
+#import "DSLCalendarView.h"
+#import "DSLCalendarDayView.h"
 
-@interface ViewController () <DSLCalendarViewDelegate>
-
+@interface ViewController ()<DSLCalendarViewDelegate>{
+    
+}
 @property (nonatomic, weak) IBOutlet DSLCalendarView *calendarView;
+//@property (nonatomic, weak) DSLCalendarView *calendarView;
 
 @end
-
 @implementation ViewController
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     
-    self.calendarView.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
@@ -39,41 +41,27 @@
 
 #pragma mark - DSLCalendarViewDelegate methods
 
-- (void)calendarView:(DSLCalendarView *)calendarView didSelectRange:(DSLCalendarRange *)range {
+- (void)calendarView:(DSLCalendarView *)calendarView didSelectRange:(DSLCalendarRange *)range{
     if (range != nil) {
-        NSLog( @"Selected %ld/%ld - %ld/%ld", (long)range.startDay.day, (long)range.startDay.month, (long)range.endDay.day, (long)range.endDay.month);
     }
     else {
-        NSLog( @"No selection" );
     }
 }
 
-- (DSLCalendarRange*)calendarView:(DSLCalendarView *)calendarView didDragToDay:(NSDateComponents *)day selectingRange:(DSLCalendarRange *)range {
-    if (NO) { // Only select a single day
-        return [[DSLCalendarRange alloc] initWithStartDay:day endDay:day];
-    }
-    else if (NO) { // Don't allow selections before today
-        NSDateComponents *today = [[NSDate date] dslCalendarView_dayWithCalendar:calendarView.visibleMonth.calendar];
-        
-        NSDateComponents *startDate = range.startDay;
-        NSDateComponents *endDate = range.endDay;
-        
-        if ([self day:startDate isBeforeDay:today] && [self day:endDate isBeforeDay:today]) {
-            return nil;
-        }
-        else {
-            if ([self day:startDate isBeforeDay:today]) {
-                startDate = [today copy];
-            }
-            if ([self day:endDate isBeforeDay:today]) {
-                endDate = [today copy];
-            }
-            
-            return [[DSLCalendarRange alloc] initWithStartDay:startDate endDay:endDate];
-        }
-    }
+- (BOOL)calendarView:(DSLCalendarView *)calendarView shouldSelectDate:(NSDateComponents *)dateComponents {
+    return YES;
     
-    return range;
+}
+
+- (BOOL)calendarView:(DSLCalendarView *)calendarView shouldDeselectDate:(NSDateComponents *)dateComponents {
+    return YES;
+}
+
+-(void)calendarView:(DSLCalendarView *)calendarView didSelectDate:(NSDateComponents *)date {
+}
+
+-(void)calendarView:(DSLCalendarView *)calendarView didDeselectDate:(NSDateComponents *)date {
+    
 }
 
 - (void)calendarView:(DSLCalendarView *)calendarView willChangeToVisibleMonth:(NSDateComponents *)month duration:(NSTimeInterval)duration {
@@ -81,11 +69,6 @@
 }
 
 - (void)calendarView:(DSLCalendarView *)calendarView didChangeToVisibleMonth:(NSDateComponents *)month {
-    NSLog(@"Now showing %@", month);
+    NSLog(@"Now showing ");
 }
-
-- (BOOL)day:(NSDateComponents*)day1 isBeforeDay:(NSDateComponents*)day2 {
-    return ([day1.date compare:day2.date] == NSOrderedAscending);
-}
-
 @end
